cd67b6ac33ed04cd3d15c646111da0c62072a66c
FelipeMCassiano <felipemcassiano@gmail.com>
2025-05-07 12:24:26 -0300

    feat: remove unuseful project

--- src/Backend/Breviori.Application/Services/UrlEncoder.cs
+++ src/Backend/Breviori.Application/Services/UrlEncoder.cs
@@ -5,12 +5,10 @@ namespace Breviori.Application.Services;
 
 public static class UrlEncoder
 {
-
 	public static string Encode(string value)
 	{
 		var bytes = Encoding.UTF8.GetBytes(value);
 		var hashString = SHA256.HashData(bytes);
-		return Convert.ToHexString(hashString)[..5];
+		return Convert.ToHexString(hashString)[..5].ToLower();
 	}
-	
 }
